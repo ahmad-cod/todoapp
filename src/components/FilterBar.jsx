2902ce84d8e2e82f078984e2cf5b89cfdb54fa74
ahmad-cod <ahmadadewale800@gmail.com>
2023-06-25 00:38:29 +0100

    Fix filter

--- src/components/FilterBar.jsx
+++ src/components/FilterBar.jsx
@@ -5,15 +5,15 @@ const FilterBar = ({ setFilter }) => {
     <div className="dark:text-dgrayishBlue todo_container justify-center gap-x-8 sm:gap-x-5 border-none">
       <a 
         className="text-blue-500 cursor-pointer"
-        onClick={() => setFilter('all')}
+        onClick={() => setFilter({ value: 'all' })}
       >All</a>
       <a 
         className="hover:text-blue-400 cursor-pointer"
-        onClick={() => setFilter('active')}
+        onClick={() => setFilter({ value: 'active' })}
       >Active</a>
       <a 
         className="hover:text-blue-400 cursor-pointer"
-        onClick={() => setFilter('completed')}
+        onClick={() => setFilter({ value: 'completed' })}
       >Completed</a>
     </div>
   )
